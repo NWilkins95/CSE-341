1efcf85fffd6c40ce44266bda1674c78149af8da
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-10-08 16:38:53 -0600

    Update Swagger API host configuration and improve documentation descriptions for clarity

--- swagger.js
+++ swagger.js
@@ -2,14 +2,12 @@ require('dotenv').config();
 
 const swaggerAutogen = require('swagger-autogen')();
 
-const isDev = process.env.NODE_ENV !== 'production';
-
 const doc = {
   info: {
     title: "Nick's Contact List API",
     description: 'A simple API to manage a contact list with CRUD operations'
   },
-  host: isDev ? 'localhost:3000' : process.env.RENDER_EXTERNAL_URL,
+  host: process.env.RENDER_EXTERNAL_URL,
   schemes: ['https']
 };
 
