510656a1c5bc0041152e8145a90b856ce0eda4dc
Nicholas Wilkins <150397998+NWilkins95@users.noreply.github.com>
2025-10-08 16:28:28 -0600

    Refactor Swagger configuration to use environment variables for host URL

--- swagger.js
+++ swagger.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
+
 const swaggerAutogen = require('swagger-autogen')();
+
+const isDev = process.env.NODE_ENV !== 'production';
 
 const doc = {
   info: {
     title: "Nick's Contact List API",
     description: 'A simple API to manage a contact list with CRUD operations'
   },
-  host: 'localhost:3000'
+  host: isDev ? 'localhost:3000' : process.env.RENDER_EXTERNAL_URL,
+  schemes: ['https']
 };
 
 const outputFile = './swagger.json';
 const routes = ['./routes/index.js'];
-
-/* NOTE: If you are using the express Router, you must pass in the 'routes' only the 
-root file where the route starts, such as index.js, app.js, routes.js, etc ... */
 
 swaggerAutogen(outputFile, routes, doc);
